bf4832aedd7d7363bc5daba0f5dde337a3e62ab7
rico kristanto <ricokristantoamansyah@gmail.com>
2019-05-15 08:14:13 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Rico Kristanto on 15/05/19.
+//  Copyright © 2019 Rico Kristanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
